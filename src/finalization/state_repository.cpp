b1dbf2b1bd4b5370f4273ec61fceb560610a70c3
Kostiantyn Stepaniuk <kostia@thirdhash.com>
2019-04-30 16:58:44 +0200

    w
    Signed-off-by: Kostiantyn Stepaniuk <kostia@thirdhash.com>

--- src/finalization/state_repository.cpp
+++ src/finalization/state_repository.cpp
@@ -185,7 +185,7 @@ bool RepositoryImpl::LoadStatesFromDB() {
       LogPrint(BCLog::FINALIZATION, "Restoring state repository from disk, last_finalized_epoch=%d\n",
                *last_finalized_epoch);
       const blockchain::Height height =
-          m_finalization_params->GetEpochCheckpointHeight(*last_finalized_epoch + 1);
+          m_finalization_params->GetEpochCheckpointHeight(*last_finalized_epoch);
       m_state_db->LoadStatesHigherThan(height, &m_states);
       if (!m_states.empty()) {
         return true;
